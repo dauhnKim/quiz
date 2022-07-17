083642cc9a7f2b34859abee81db792e8a0554b81
Dauhn <user1@Dauhn-MacBook-Pro.local>
2022-07-17 23:49:35 +0900

    test: check the data from api

--- src/pages/__tests__/MainQuiz.test.tsx
+++ src/pages/__tests__/MainQuiz.test.tsx
@@ -1,7 +1,21 @@
 import { render, screen, cleanup, waitFor } from "@testing-library/react";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { BrowserRouter } from "react-router-dom";
 import MainQuiz from "../MainQuiz";
+
+const getQuiz = async () => {
+  try {
+    const url = "https://opentdb.com/api.php?amount=2&category=27&type=multiple";
+    const resp = await axios.get(url);
+    if (resp.status !== 200) {
+      throw new Error("Something went wrong");
+    }
+    const data = await resp.data;
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -10,72 +24,61 @@ afterEach(() => {
   cleanup();
 });
 
-test("should render number field component", async () => {
-  const { container } = render(<MainQuiz />, { wrapper: BrowserRouter });
+describe("getQuiz", () => {
+  test("should return quiz list from api", async () => {
+    //Our desired output
+    const quizees = [
+      {
+        category: "Animals",
+        type: "multiple",
+        difficulty: "hard",
+        question: "Which species of Brown Bear is not extinct?",
+        correct_answer: "Syrian Brown Bear",
+        incorrect_answers: ["California Grizzly Bear", "Atlas Bear", "Mexican Grizzly Bear"],
+      },
+    ];
 
-  mockedAxios.get.mockResolvedValue({
-    data: {
-      response_code: 0,
+    //Prepare the response we want to get from axios
+    const mockedResponse: AxiosResponse = {
+      data: quizees,
+      status: 200,
+      statusText: "OK",
+      headers: {},
+      config: {},
+    };
+
+    // Make the mock return the custom axios response
+    mockedAxios.get.mockResolvedValueOnce(mockedResponse);
+    expect(axios.get).not.toHaveBeenCalled();
+    const data = await getQuiz();
+
+    expect(axios.get).toHaveBeenCalled();
+    expect(data).toEqual(quizees);
+  });
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("MainQuiz", () => {
+  test("should return quiz list from main quiz component", async () => {
+    const data = {
       results: [
         {
-          category: "Animals",
-          type: "multiple",
-          difficulty: "hard",
-          question: "Which species of Brown Bear is not extinct?",
-          correct_answer: "Syrian Brown Bear",
-          incorrect_answers: ["California Grizzly Bear", "Atlas Bear", "Mexican Grizzly Bear"],
-        },
-        {
-          category: "Animals",
-          type: "multiple",
-          difficulty: "hard",
-          question: "What is the scientific name of the cheetah?",
-          correct_answer: "Acinonyx jubatus",
-          incorrect_answers: ["Panthera onca", "Lynx rufus", "Felis catus"],
-        },
-        {
-          category: "Animals",
-          type: "multiple",
-          difficulty: "medium",
-          question: "Which animal was part of an Russian domestication experiment in 1959?",
-          correct_answer: "Foxes",
-          incorrect_answers: ["Pigeons", "Bears", "Alligators"],
-        },
-        {
-          category: "Animals",
-          type: "multiple",
-          difficulty: "easy",
-          question: "What is Grumpy Cat&#039;s real name?",
-          correct_answer: "Tardar Sauce",
-          incorrect_answers: ["Sauce", "Minnie", "Broccoli"],
-        },
-        {
-          category: "Animals",
-          type: "multiple",
-          difficulty: "hard",
-          question: "Which species is a &quot;mountain chicken&quot;?",
-          correct_answer: "Frog",
-          incorrect_answers: ["Chicken", "Horse", "Fly"],
+          question: "What was the name of the Ethiopian Wolf before they knew it was related to wolves?",
+          correct_answer: "Simien Jackel",
+          incorrect_answers: ["Ethiopian Coyote", "Amharic Fox", "Canis Simiensis"],
         },
       ],
-    },
-  });
+    };
+    mockedAxios.get.mockResolvedValue({ data: data });
 
-  // need to test axios function
-  await waitFor(() => {
-    // Provide the data object to be returned
-    // mockedAxios.get.mockResolvedValue({
-    //   data: [{ correct_answer: "", incorrect_answers: [], question: "" }],
-    // });
-    // const userList = getAllByRole("listitem");
-    // expect(userList).toHaveLength(2);
-    // expect(userList[0]).toHaveTextContent("Joe Doe");
-    // expect(userList[1]).toHaveTextContent("Jane Doe");
-  });
+    render(<MainQuiz />, { wrapper: BrowserRouter });
 
-  await waitFor(() => {
-    const groupRadio = container.querySelector(".py-10");
-    expect(groupRadio).toBeInTheDocument();
-    // expect(groupRadio).toHaveTextContent(/atlas bea/i);
+    await waitFor(() => {
+      const question = screen.getByText(/What/i);
+      expect(question).toBeInTheDocument();
+    });
   });
 });
